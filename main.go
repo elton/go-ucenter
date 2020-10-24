23ccb9eaabd8bb41f3ec8de23d9fdaf3e3584a9a
Elton Zheng <elton.zheng.sh@gmail.com>
2020-10-24 23:00:41 +0800

    Add send sms function.

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	address := ":8080"
 
 	log.Printf("process with pid %d serving %s.\n", pid, address)
+	// gracehttp is a simple and graceful HTTP server for Golang.
+	// kill -SIGUSR2 $pid 重启服务器
+	// kill -SIGTERM $pid 关闭服务器
 	err := gracehttp.ListenAndServe(address, routes.New())
 	if err != nil {
 		log.Printf("process with pid %d stoped, error: %s.\n", pid, err)
